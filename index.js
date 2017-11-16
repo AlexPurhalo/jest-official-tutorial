2832374dbe1f7f2d675d460070a5f1c35c5215df
AlexPurhalo <alexpurhalo@gmail.com>
2017-11-16 23:44:55 +0200

    DOM testing

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import React    from 'react'
 import ReactDOM from 'react-dom'
 
-import Link from './src/components/Link'
+import Link              from './src/components/Link'
+import CheckboxWithLabel from './src/components/CheckboxWithLabel'
 
 const App = () => (
 	<div>
 		<Link page="http://www.facebook.com">
 			Facebook
 		</Link>
+		<CheckboxWithLabel
+			labelOff="Off"
+			labelOn="On"
+		/>
 	</div>
 )
 
